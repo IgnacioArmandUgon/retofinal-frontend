5b65b4b28abd915be44466ad5d8c19b3cf1d4ff2
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-04-01 18:21:13 -0300

    repo para eddi

--- src/components/FormVolante.jsx
+++ src/components/FormVolante.jsx
@@ -3,9 +3,12 @@ import { URL } from "../constantes";
 import { useState, useRef } from "react";
 import NavForm from "./NavFrom";
 const FormVolante = () => {
+  const [productos, setProductos] = useState([]);
+
   const proveedorRef = useRef("");
-  const productosRef = useRef([{}]);
+  const productosRef = useRef([{nombreProducto: "", cantidad : 0}]);
   const idProveedorRef = useRef(0);
+  const cantidadRef = useRef(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,30 +21,40 @@ const FormVolante = () => {
       },
       body: JSON.stringify({
         nombreProveedor: proveedorRef.current.value,
-        productos: [{nombreProducto: productosRef.current.value}],
-        idProveedor: idProveedorRef.current.value
+        //productos: productos,
+        idProveedor: idProveedorRef.current.value,
       }),
     });
   };
-    return (
-      <>
-      <NavForm/>
-      <h3 className=" m-4">Registre sus volantes</h3>
-        <form className="form m-4"  onSubmit={handleSubmit}>
-          <input type="text" ref={proveedorRef} />
-          <label>Nombre proveedor</label>
-          <p></p>
-          <input type="text" ref={productosRef} />
-          <label>Productos</label>
-          <p></p>
-          <input type="number" ref={idProveedorRef} />
-          <label>Documento del proveedor</label>
-          <p></p>
-          <button className="btn btn-primary" type="submit">Enviar</button>
-        </form>
-      </>
-    );
-  };
 
+  const handleAgregar = (e) => {
+    e.preventDefault()
+    setProductos(previousState => { return { ...previousState, nombreProducto: productosRef.current.value, cantidad: cantidadRef.current.value}})
+    console.log(productos)
+  }
+  return (
+    <>
+      <NavForm />
+      <h3 className=" m-4">Registre sus volantes</h3>
+      <form className="form m-4" onSubmit={handleSubmit}>
+        <input type="text" ref={proveedorRef} />
+        <label>Nombre proveedor</label>
+        <p></p>
+        <input type="text" ref={productosRef} />
+        <label>Productos</label>
+        <input type="number" ref={cantidadRef}/>
+        <label>Cantidad</label>
+        <button onClick={(e) => handleAgregar(e)}>Agregar</button>
+        <p></p>
+        <input type="number" ref={idProveedorRef} />
+        <label>Documento del proveedor</label>
+        <p></p>
+        <button className="btn btn-primary" type="submit">
+          Enviar
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default FormVolante;
