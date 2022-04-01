63e3cfe6587824b23985071e2e93fe5232506806
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-04-01 00:12:15 -0300

    metodo POST funciona

--- src/components/CrearFactura.jsx
+++ src/components/CrearFactura.jsx
@@ -3,7 +3,6 @@ import { URL } from "../constantes";
 import { useState, useRef } from "react";
 
 const CrearFactura = () => {
-  //const [factura, setFactura] = useState({nombreCliente: "", empleadoQueAtendio: "", precioTotal: 0})
   const clienteRef = useRef("");
   const empleadoRef = useRef("");
   const precioRef = useRef(0);
@@ -11,44 +10,33 @@ const CrearFactura = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const request= {
-      nombreCliente: clienteRef.current.value,
-      empleadoQueAtendio: empleadoRef.current.value,
-      precioTotal: precioRef.current.value
-    }
-
     fetch(`${URL}/facturas`, {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(request),
-    })
-
+      body: JSON.stringify({
+        nombreCliente: clienteRef.current.value,
+        empleadoQueAtendio: empleadoRef.current.value,
+        precioTotal: precioRef.current.value
+      }),
+    });
+  };
     return (
       <>
-        <form onSubmit={(e) => handleSubmit()}>
-          <input
-            type="text"
-            ref={clienteRef}
-          />
+        <form onSubmit={handleSubmit}>
+          <input type="text" ref={clienteRef} />
           <label>Nombre cliente</label>
-          <input
-            type="text"
-            ref={empleadoRef}
-          />
+          <input type="text" ref={empleadoRef} />
           <label>Nombre empleado</label>
-          <input
-            type="number"
-            ref={precioRef}
-          />
+          <input type="number" ref={precioRef} />
           <label>Precio total</label>
-          <input type="submit"/>
+          <button type="submit">Enviar</button>
         </form>
       </>
     );
   };
-};
+
 
 export default CrearFactura;
